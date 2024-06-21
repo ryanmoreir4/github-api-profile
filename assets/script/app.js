5478abe3f33666a41b15bb45d166ac35f2741554
Ryan Moreira <rmpoliveira@sga.pucminas.br>
2024-06-21 08:29:58 -0300

    adicionando tela de repositorio

--- assets/script/app.js
+++ assets/script/app.js
@@ -1,43 +1,49 @@
-const repositorios = document.querySelector(".card-container");
-const colegasTrabalho = document.querySelector(".colegastrabalho");
-const perfilImg = document.querySelector(".imgprofile");
-const perfilName = document.getElementById("profile-name");
-const perfilBio = document.getElementById("profile-bio");
-const perfilLocation = document.getElementById("profile-location");
-const perfil = document.querySelector(".perfil-me");
+const token = `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`;
+const headers = { Authorization: `token ${token}` };
 
-function getApiGitHub() {
-  //Puxar dados do perfil do GitHub
-  fetch("https://api.github.com/users/ryanmoreir4")
-    .then(async (res) => {
-      if (!res.ok) {
-        throw new Error(res.status);
-      }
-      let userData = await res.json();
-      perfilImg.src = userData.avatar_url;
-      perfilImg.alt = `${userData.name} Profile Picture`;
-      perfilName.textContent = userData.name;
-      perfilBio.textContent = userData.bio;
-      perfilLocation.innerHTML = `<b>Location:</b> ${userData.location}`;
-    })
-    .catch((error) => {
-      console.error("Erro ao buscar informações do usuário:", error);
-    });
+document.addEventListener("DOMContentLoaded", function () {
+  const repositorios = document.querySelector(".card-container");
+  const colegasTrabalho = document.querySelector(".colegastrabalho");
+  const perfilImg = document.querySelector(".imgprofile");
+  const perfilName = document.getElementById("profile-name");
+  const perfilBio = document.getElementById("profile-bio");
+  const perfilLocation = document.getElementById("profile-location");
 
-  //Puxar repositórios
-  fetch("https://api.github.com/users/ryanmoreir4/repos")
-    .then(async (res) => {
-      if (!res.ok) {
-        throw new Error(res.status);
-      }
-      let data = await res.json();
-      data.forEach((item) => {
-        let project = document.createElement("div");
-        project.classList.add("card-content");
+  function getApiGitHub() {
+    //Puxar dados do perfil do GitHub
+    fetch("https://api.github.com/users/ryanmoreir4", { headers })
+      .then(async (res) => {
+        if (!res.ok) {
+          throw new Error(res.status);
+        }
+        let userData = await res.json();
+        perfilImg.src = userData.avatar_url;
+        perfilImg.alt = `${userData.name} Profile Picture`;
+        perfilName.textContent = userData.name;
+        perfilBio.textContent = userData.bio;
+        perfilLocation.innerHTML = `<b>Location:</b> ${userData.location}`;
+      })
+      .catch((error) => {
+        console.error("Erro ao buscar informações do usuário:", error);
+      });
 
-        project.innerHTML = `
+    //Puxar repositórios
+    fetch("https://api.github.com/users/ryanmoreir4/repos", { headers })
+      .then(async (res) => {
+        if (!res.ok) {
+          throw new Error(res.status);
+        }
+        let data = await res.json();
+        data.forEach((item) => {
+          let project = document.createElement("div");
+          project.classList.add("card-content");
+          project.addEventListener("click", () => {
+            window.location.href = `repo.html?id=${item.id}`;
+          });
+
+          project.innerHTML = `
           <p class="title-card">
-            <a style="color: black" href="repo.html">
+            <a style="color: black" ">
               <b>${item.name}</b>
             </a>
           </p>
@@ -45,39 +51,40 @@ function getApiGitHub() {
           <div class="icon-card-repo">
             <img src="/assets/img/grande-estrela-favorita.png" alt="Star Icon" />
             <p>${item.stargazers_count}</p>
-            <img src="/assets/img/sombra-de-usuario-masculino.png" alt="Fork Icon" />
+            <img src="/assets/img/fork-github.png" alt="Fork Icon" />
             <p>${item.forks_count}</p>
           </div>`;
-        repositorios.appendChild(project);
+          repositorios.appendChild(project);
+        });
+      })
+      .catch((error) => {
+        console.error("Erro ao buscar informações:", error);
       });
-    })
-    .catch((error) => {
-      console.error("Erro ao buscar informações:", error);
-    });
 
-  //Puxar colegas de trabalho
-  fetch("https://api.github.com/users/ryanmoreir4/following")
-    .then(async (res) => {
-      if (!res.ok) {
-        throw new Error(res.status);
-      }
-      let collabsData = await res.json();
-      colegasTrabalho.innerHTML = "<h2>Colegas de Trabalho</h2>";
-      collabsData.forEach((collab) => {
-        let colega = document.createElement("figure");
-        colega.classList.add("perfil-colega");
+    //Puxar colegas de trabalho
+    fetch("https://api.github.com/users/ryanmoreir4/following", { headers })
+      .then(async (res) => {
+        if (!res.ok) {
+          throw new Error(res.status);
+        }
+        let collabsData = await res.json();
+        colegasTrabalho.innerHTML = "<h2>Colegas de Trabalho</h2>";
+        collabsData.forEach((collab) => {
+          let colega = document.createElement("figure");
+          colega.classList.add("perfil-colega");
 
-        colega.innerHTML = `
+          colega.innerHTML = `
             <a style="color: black" href="https://github.com/${collab.login}">
             <img src="${collab.avatar_url}" alt="${collab.login}" />
             <figcaption>${collab.login}</figcaption></a
             >`;
-        colegasTrabalho.appendChild(colega);
+          colegasTrabalho.appendChild(colega);
+        });
+      })
+      .catch((error) => {
+        console.error("Erro ao buscar colaboradores do repositório:", error);
       });
-    })
-    .catch((error) => {
-      console.error("Erro ao buscar colaboradores do repositório:", error);
-    });
-}
+  }
 
-getApiGitHub();
+  getApiGitHub();
+});
---
+++ assets/script/repo.js
@@ -0,0 +1,46 @@
+const token = `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`;
+const headers = { Authorization: `token ${token}` };
+
+document.addEventListener("DOMContentLoaded", function () {
+  const repoName = document.querySelector(".repo-name");
+  const repoDescription = document.querySelector(".repo-description");
+  const repoDate = document.querySelector(".repo-date");
+  const repoLanguage = document.querySelector(".repo-language");
+  const repoLink = document.querySelector(".repo-link");
+
+  // Função para pegar os parâmetros da URL
+  function getQueryParams() {
+    const params = new URLSearchParams(window.location.search);
+    return {
+      id: params.get("id"),
+    };
+  }
+
+  // Pega o ID do repositório da URL
+  const { id } = getQueryParams();
+
+  if (id) {
+    // Busca as informações do repositório pelo ID
+    fetch(`https://api.github.com/repositories/${id}`, { headers })
+      .then(async (res) => {
+        if (!res.ok) {
+          throw new Error(res.status);
+        }
+        let repoData = await res.json();
+        repoName.textContent = `Repositório: ${repoData.name}`;
+        repoDescription.textContent =
+          repoData.description || "Descrição não disponível.";
+        repoDate.textContent = new Date(repoData.created_at).toLocaleDateString(
+          "pt-BR"
+        );
+        repoLanguage.textContent =
+          repoData.languages_url || "Linguagem não disponível.";
+        repoLink.innerHTML = `<a href="https://github.com/${repoData.login}/${repoData.name}" target="_blank">${data.html_url}</a>`;
+      })
+      .catch((error) => {
+        console.error("Erro ao buscar informações do repositório:", error);
+      });
+  } else {
+    console.error("ID do repositório não encontrado na URL");
+  }
+});
